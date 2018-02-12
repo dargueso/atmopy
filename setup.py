36edff83d9fd80d37149583fc85759fe5152f928
dargueso <d.argueso@unsw.edu.au>
2018-02-12 16:31:27 +1100

    Initialize package and git repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='atmopy',
+      version='0.1',
+      description='Package to plot and process atmospheric data',
+      url='http://github.com/storborg/funniest',
+      author='Daniel Argueso',
+      author_email='d.argueso@uib.es',
+      license='MIT',
+      packages=['atmopy'],
+      zip_safe=False)
